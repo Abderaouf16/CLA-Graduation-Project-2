2bbfff98a65120b914681606fc6df52e24e8a5fe
Abderaouf16 <abderaoufa88@gmail.comm>
2024-07-30 19:36:13 +0100

    Feat: edited app structure

--- src/app/(routes)/Home/page.tsx
+++ src/app/(routes)/Home/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Header from '../Components/Header/Header'
+import Home from './Home'
 function page() {
   return (
     <>
     <div className=" px-4 md:px-8 xl:px-14">
-    <Header />
+      <Home/>
     </div>
     </>
   )
