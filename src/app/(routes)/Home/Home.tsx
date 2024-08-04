6e82b5daf255c1b356d81577cd5c864087ed884e
Abderaouf16 <abderaoufa88@gmail.comm>
2024-08-04 18:48:27 +0100

    Feat: add styling to header and the overall layout

--- src/app/(routes)/Home/Home.tsx
+++ src/app/(routes)/Home/Home.tsx
@@ -4,8 +4,8 @@ import "./Home.css";
 function Page() {
   return (
     <>
-      <div className="px-4 md:px-6 xl:px-14 ">
         <Header />
+      <div className="px-4 md:px-6 xl:px-14 py-16 ">
         <div
           className="bg-slate-400 w-full h-[550px] rounded-2xl my-10  relative"
           style={{
@@ -14,19 +14,26 @@ function Page() {
             backgroundPosition: "center",
           }}
         >
-          <div className="blurContainer bg-slate-500  w-[350px] h-[150px]  rounded-2xl  absolute bottom-0 left-0 m-5 p-3">
-            <div className=" bg-red-400 flex ">
+          <div className="blurContainer bg-slate-500  w-[370px]  rounded-2xl  absolute bottom-0 left-0 m-5 py-3 px-5">
+            <div className="  flex ">
               <div className="heroPofileImg"></div>
-              <div className=" mx-3">
-                <h1 className=' text-sm text-white font-bold '>Anderson chavvaria</h1>
-                <p className=' text-xs text-white font-base'>@andrs</p>
+              <div className=" mx-3 ">
+                <h1 className=" text-sm text-white font-semibold ">
+                  Anderson chavvaria
+                </h1>
+                <p className="textGrey  text-xs font-base">@andrs</p>
               </div>
             </div>
-            <div className=" bg-blue-800">
-              <h1 className='text-white text-3xl'>Modern luxury living  space revealed</h1>
+            <div className="  my-2">
+              <h1 className="text-white text-xl font-semibold">
+                Modern luxury living space revealed
+              </h1>
             </div>
-            <div className="bg-green-500">
-              <h1>fff</h1>
+            <div className=" my-2">
+              <p className="text-xs textGrey">
+                Discover the epitome of luxury living in this modern space.
+                Elegance meets comfort with hight ceiling, natural light
+              </p>
             </div>
           </div>
         </div>
--- src/app/(routes)/Home/page.tsx
+++ src/app/(routes)/Home/page.tsx
@@ -3,7 +3,7 @@ import Home from './Home'
 function page() {
   return (
     <>
-    <div className=" px-4 md:px-8 xl:px-14">
+    <div className=" ">
       <Home/>
     </div>
     </>
