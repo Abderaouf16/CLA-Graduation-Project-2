8378c59111dd1eec835aae5c34c5ae075d10240c
Abderaouf16 <abderaoufa88@gmail.comm>
2024-08-07 22:35:26 +0100

    Feat: add project grid

--- src/app/(routes)/Home/Home.tsx
+++ src/app/(routes)/Home/Home.tsx
@@ -3,7 +3,6 @@
 import React, { useState } from "react";
 import Header from "../../Components/Header/Header";
 import "./Home.css";
-import bars from "../../../../public/bars-solid.svg";
 import Image from "next/image";
 
 import {
@@ -17,9 +16,8 @@ import {
 } from "@/components/ui/menubar";
 
 function Page() {
-
-//  Filters
-const [activeIndex, setActiveIndex] = useState <number | null>(0);
+  // Filters
+  const [activeIndex, setActiveIndex] = useState<number | null>(0);
   const buttons = [
     "interieur",
     "Landscape & Urbanism",
@@ -28,10 +26,6 @@ const [activeIndex, setActiveIndex] = useState <number | null>(0);
     "Residental",
   ];
 
-
-
-
-
   return (
     <>
       <Header />
@@ -39,78 +33,120 @@ const [activeIndex, setActiveIndex] = useState <number | null>(0);
         <div
           className="bg-slate-400 w-full h-[550px] rounded-2xl my-10  relative"
           style={{
-            backgroundImage: `url(/heroImg.jpg)`, // Public path reference
+            backgroundImage: `url(/heroImg.jpg)`,
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
         >
-          <div className="blurContainer bg-slate-500   md:w-[370px]   rounded-2xl  absolute bottom-0 left-0 m-5 py-3 px-5">
-            <div className="  flex ">
+          <div className="blurContainer bg-slate-500 md:w-[370px] rounded-2xl absolute bottom-0 left-0 m-5 py-3 px-5">
+            <div className="flex">
               <div className="heroPofileImg"></div>
-              <div className=" mx-3 ">
-                <h1 className=" text-xs md:text-sm text-white font-semibold ">
+              <div className="mx-3">
+                <h1 className="text-xs md:text-sm text-white font-semibold">
                   Anderson chavvaria
                 </h1>
-                <p className="textGrey  text-xs  font-base">@andrs</p>
+                <p className="textGrey text-xs font-base">@andrs</p>
               </div>
             </div>
-            <div className="  my-2">
+            <div className="my-2">
               <h1 className="text-white text-lg md:text-xl font-semibold">
                 Modern luxury living space revealed
               </h1>
             </div>
-            <div className=" my-2">
-              <p className=" text-xs md:text-xs textGrey">
+            <div className="my-2">
+              <p className="text-xs md:text-xs textGrey">
                 Discover the epitome of luxury living in this modern space.
-                Elegance meets comfort with hight ceiling, natural light
+                Elegance meets comfort with high ceilings, natural light
               </p>
             </div>
           </div>
         </div>
 
         <section>
-        <div className=" h-16 flex      ">
-          <div className=" flex-1 flex items-center flex-wrap">
-          {buttons.map((text, index) => (
-            <button
-              key={index}
-              className={` ${
-                activeIndex === index ? "bg-[#EBEBEB]" : "bg-white"
-              } px-7 mx-2 text-xs md:text-sm font-medium cursor-pointer rounded-xl border-2 border-[#EBEBEB]  py-5 h-10 flex justify-center items-center my-2  `}
-              onClick={() => setActiveIndex(index)}
-            >
-              {text}
-            </button>
-          ))}
+          <div className="h-16 flex">
+            <div className="flex-1 flex items-center flex-wrap">
+              {buttons.map((text, index) => (
+                <button
+                  key={index}
+                  className={`${
+                    activeIndex === index ? "bg-[#EBEBEB]" : "bg-white"
+                  } px-7 mx-2 text-xs md:text-sm font-medium cursor-pointer rounded-xl border-2 border-[#EBEBEB] py-5 h-10 flex justify-center items-center my-2`}
+                  onClick={() => setActiveIndex(index)}
+                >
+                  {text}
+                </button>
+              ))}
+            </div>
 
-          </div>
-         
-          <div className="   flex ">
-            <Menubar className="rounded-xl border-2 border-[#EBEBEB]  cursor-pointer">
-              <MenubarMenu >
-                <MenubarTrigger className=' cursor-pointer'>
-                  <Image
-                    src="/bars-solid.svg"
-                    alt="Menu Icon"
-                    width={15}
-                    height={5}
-                  />
-                </MenubarTrigger>
-                <MenubarContent>
-                  <MenubarItem>
-                    New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-                  </MenubarItem>
-                  <MenubarItem>New Window</MenubarItem>
-                  <MenubarSeparator />
-                  <MenubarItem>Share</MenubarItem>
-                  <MenubarSeparator />
-                  <MenubarItem>Print</MenubarItem>
-                </MenubarContent>
-              </MenubarMenu>
-            </Menubar>{" "}
-          </div>
+            <div className="flex">
+              <Menubar className="rounded-xl border-2 border-[#EBEBEB] cursor-pointer">
+                <MenubarMenu>
+                  <MenubarTrigger className="cursor-pointer">
+                    <Image
+                      src="/bars-solid.svg"
+                      alt="Menu Icon"
+                      width={15}
+                      height={5}
+                    />
+                  </MenubarTrigger>
+                  <MenubarContent>
+                    <MenubarItem>
+                      New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+                    </MenubarItem>
+                    <MenubarItem>New Window</MenubarItem>
+                    <MenubarSeparator />
+                    <MenubarItem>Share</MenubarItem>
+                    <MenubarSeparator />
+                    <MenubarItem>Print</MenubarItem>
+                  </MenubarContent>
+                </MenubarMenu>
+              </Menubar>
+            </div>
           </div>
 
+          <div className="row">
+            <div className="column">
+            <img src="https://www.w3schools.com/w3images/wedding.jpg" style={{ width: "100%" }} /> 
+            <img src="https://www.w3schools.com/w3images/rocks.jpg" style={{ width: "100%" }} /> 
+            <img src="https://www.w3schools.com/w3images/falls2.jpg" style={{ width: "100%" }} /> 
+            <img src="https://www.w3schools.com/w3images/paris.jpg" style={{ width: "100%" }} /> 
+            <img src="https://www.w3schools.com/w3images/nature.jpg" style={{ width: "100%" }} /> 
+            <img src="https://www.w3schools.com/w3images/mist.jpg" style={{ width: "100%" }} /> 
+            <img src="https://www.w3schools.com/w3images/paris.jpg" style={{ width: "100%" }} /> 
+
+
+            </div>
+
+            <div className="column">
+            <img src="https://www.w3schools.com/w3images/underwater.jpg" style={{ width: "100%" }} /> 
+            <img src="https://www.w3schools.com/w3images/ocean.jpg" style={{ width: "100%" }} /> 
+            <img src="https://www.w3schools.com/w3images/wedding.jpg" style={{ width: "100%" }} /> 
+            <img src="https://www.w3schools.com/w3images/mountainskies.jpg" style={{ width: "100%" }} /> 
+            <img src="https://www.w3schools.com/w3images/rocks.jpg" style={{ width: "100%" }} /> 
+            <img src="https://www.w3schools.com/w3images/underwater.jpg" style={{ width: "100%" }} /> 
+
+            </div>
+
+            <div className="column">
+            <img src="https://www.w3schools.com/w3images/wedding.jpg" style={{ width: "100%" }} /> 
+            <img src="https://www.w3schools.com/w3images/rocks.jpg" style={{ width: "100%" }} /> 
+            <img src="https://www.w3schools.com/w3images/falls2.jpg" style={{ width: "100%" }} /> 
+            <img src="https://www.w3schools.com/w3images/paris.jpg" style={{ width: "100%" }} /> 
+            <img src="https://www.w3schools.com/w3images/nature.jpg" style={{ width: "100%" }} /> 
+            <img src="https://www.w3schools.com/w3images/mist.jpg" style={{ width: "100%" }} /> 
+            <img src="https://www.w3schools.com/w3images/paris.jpg" style={{ width: "100%" }} /> 
+
+            </div>
+
+            <div className="column">
+            <img src="https://www.w3schools.com/w3images/underwater.jpg" style={{ width: "100%" }} /> 
+            <img src="https://www.w3schools.com/w3images/ocean.jpg" style={{ width: "100%" }} /> 
+            <img src="https://www.w3schools.com/w3images/wedding.jpg" style={{ width: "100%" }} /> 
+            <img src="https://www.w3schools.com/w3images/mountainskies.jpg" style={{ width: "100%" }} /> 
+            <img src="https://www.w3schools.com/w3images/rocks.jpg" style={{ width: "100%" }} /> 
+            <img src="https://www.w3schools.com/w3images/underwater.jpg" style={{ width: "100%" }} /> 
+            </div>
+          </div>
         </section>
       </div>
     </>
